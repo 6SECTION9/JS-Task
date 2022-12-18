bd72a3d93812d8406634bce871b6d6bb1fcb87a8
عبدالغفار امحمد أبوقراض <a.abogarrad@uot.edu.ly>
2022-12-17 16:50:11 -0800

    first commit

---
+++ main.js
@@ -0,0 +1,26 @@
+var h1 = "Abdalghaffar Abogarrad(SECTION)"
+var h2 = "Web Devolober"
+var p = "My name is Abdalghaffar Abogarrad A.K.A SECTION I'm a beginner web devolober"
+
+var num1 = 1
+var num2 = 2
+var num3 = 3
+var num4 = 4
+var num5 = 5
+var num6 = 6
+var num7 = 7
+var num8 = 8
+var num9 = 9
+var num10 = 10
+
+var sum = num1 + num2 + num3 + num4 + num5 + num6 + num7 + num8 + num9 + num10
+console.log(sum)
+
+var avr = sum / 10
+console.log(avr)
+
+document.getElementById("name").innerHTML = h1
+document.getElementById("jop").innerHTML = h2
+document.getElementById("info").innerHTML = p
+document.getElementById("point").innerHTML = avr
+console.log(h1)
